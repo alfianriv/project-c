1f057e45be991eaad1214eaf05bcd75da9bdf579
alfianriv <alfian.rivaldi8@gmail.com>
2022-09-16 17:45:24 +0700

    update modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { DatabaseModule } from './config/database/database.module';
 import { UserAccessModule } from './modules/user-access/user-access.module';
-import { RoleModule } from './modules/role/role.module';
-import { MenuAccessModule } from './modules/menu-access/menu-access.module';
-import { ModuleModule } from './modules/module/module.module';
-import { MenuModuleModule } from './modules/menu-module/menu-module.module';
-import { AccessTokenModule } from './modules/access-token/access-token.module';
-import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [DatabaseModule, UserAccessModule],
